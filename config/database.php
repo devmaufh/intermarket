358d6e6dfe133b9ce187ec98bd8d2c88bf630214
Mauricio Flores Hernández <mau1361317@gmail.com>
2018-12-03 23:48:01 -0600

    configDB changed

--- config/database.php
+++ config/database.php
@@ -41,11 +41,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => env('DB_HOST', 'er7lx9km02rjyf3n.cbetxkdyhwsb.us-east-1.rds.amazonaws.com	'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'dh7ldsmq8z8n4hwi'),
+            'username' => env('DB_USERNAME', 'mhb9yc2m8xkk94v7'),
+            'password' => env('DB_PASSWORD', 'pha9tvgb1s2k17e3'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
